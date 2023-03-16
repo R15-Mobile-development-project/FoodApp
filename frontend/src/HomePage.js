5cf292775a5961f68b921a8514205bfa503384ed
JoniPahikainen <joni.pahikainen01@gmail.com>
2023-03-17 00:15:37 +0200

    basic drawer and header

---
+++ frontend/src/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function HomePage() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Page</Text>
+    </View>
+  );
+}
+
+export default HomePage;
---
+++ frontend/src/SettingsPage.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+
+function SettingsPage() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>SettingsPage</Text>
+    </View>
+  );
+}
+export default SettingsPage;
