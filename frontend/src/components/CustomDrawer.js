43b6a82f563ba361c0b77eb49b44b192e67026bb
pöggers pc <t1huhi00@students.oamk.fi>
2023-03-21 22:03:21 +0200

    add wallet icon

--- frontend/src/components/CustomDrawer.js
+++ frontend/src/components/CustomDrawer.js
@@ -5,8 +5,12 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 import COLORS from '../conts/colors';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const CustomDrawer = props => {
+  const navigation = useNavigation();
   return (
     <DrawerContentScrollView
       {...props}
@@ -20,15 +24,21 @@ const CustomDrawer = props => {
           }}>
           User Name
         </Text>
-        <View style={{flexDirection: 'row'}}>
-          {/* TODO: get balance from backend */}
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+        }}>
           <Text
             style={{
-              marginRight: 5,
+              fontSize: 15,
+              marginRight: 130,
               color: COLORS.quaternary,
             }}>
             280€
           </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Wallet')}>
+            <Icon name="wallet" size={30} color={COLORS.quaternary} />
+          </TouchableOpacity>
         </View>
       </View>
       <View
