56119ad3918c321d113f76e36370c140d5f93c04
Keson <kesonjus@gmail.com>
2023-03-23 20:17:35 +0200

    add user login and register functionality

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/userRoutes');
 
 var app = express();
 
@@ -20,15 +20,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ backend/controllers/userController.js
@@ -0,0 +1,79 @@
+const user = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const emailvalidator = require("email-validator");
+const jwt = require("../config/jwt");
+require("dotenv").config();
+
+const userLogin = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if (emailvalidator.validate(email)) {
+        user.getByEmail(email, (err, results) => {
+            if (err) {
+                res.status(500).json({
+                    message: "Error occured",
+                    error: err
+                });
+            } else {
+                if (results.length > 0) {
+                    bcrypt.compare(password, results[0].password.toString(), (err, result) => {
+                        if (err) {
+                            console.log(results[0].password);
+                            res.status(500).json({
+                                message: "Error occured",
+                                error: err
+                            });
+                        } else {
+                            if (result) {
+                                const token = jwt.generateToken(results[0].user_id);
+                                console.log("Created token: " + token);
+                                res.status(200).json({
+                                    message: "Login successful",
+                                    token: token
+                                });
+                            } else {
+                                res.status(401).json({
+                                    message: "Invalid credentials"
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    res.status(401).json({
+                        message: "Invalid credentials"
+                    });
+                }
+            }
+        });
+    } else {
+        res.status(400).json({
+            message: "Invalid email"
+        });
+    }
+}
+
+const userRegister = (req, res) => {
+    if (emailvalidator.validate(req.body.email)) {
+        user.add(req, (err, results) => {
+            if (err) {
+                res.status(500).json({
+                    message: "Error occured",
+                    error: err
+                });
+            } else {
+                res.status(200).json({
+                    message: "Registration successful"
+                });
+            }
+        });
+    } else {
+        res.status(400).json({
+            message: "Invalid email"
+        });
+    }
+}
+
+module.exports = {
+    userLogin,
+    userRegister
+}
