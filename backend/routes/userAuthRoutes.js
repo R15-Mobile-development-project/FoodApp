8b761564188406998eb8643fd9738489427fe922
Lasse Suomela <suomelasse@gmail.com>
2023-03-26 14:33:46 +0300

    removed unused imports from userRoutes. Changed var to const in routes folder

--- backend/routes/userAuthRoutes.js
+++ backend/routes/userAuthRoutes.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get('/', userController.userProfile);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var router = express.Router();
-const db = require("../config/db");
+const express = require('express');
+const router = express.Router();
 const userController = require("../controllers/userController");
-const jwt = require("../config/jwt");
 
 router.post('/login', userController.userLogin);
 router.post('/register', userController.userRegister);
