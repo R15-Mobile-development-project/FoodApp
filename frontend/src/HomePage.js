18f73dd16b53e30fc32443161983c014df6b7dec
Tommukka <laine.tomi01@gmail.com>
2023-03-28 20:10:57 +0300

    Restaurant card template

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {COLORS} from './conts/colors';
-import {ThemeContext} from './components/ThemeContext';
-import {useContext} from 'react';
-import styles from './conts/Styles';
+import React from "react";
+import {View, Text, Image} from "react-native";
+import COLORS from "./conts/colors";
+import styles from "./conts/Styles";
+import {Card, ListItem, Button, Icon} from "@rneui/themed";
 
 function HomePage() {
-  const {theme} = useContext(ThemeContext);
+  //const {theme} = useContext(ThemeContext);
   return (
-    <View
-      style={[styles.container, {backgroundColor: COLORS[theme].quaternary}]}>
-      <Text
+    <Card containerStyle={{}} wrapperStyle={{}}>
+      <Card.Title>RESTAURANT NAME</Card.Title>
+      <Card.Divider />
+      <View
         style={{
-          color: COLORS[theme].primary,
+          position: "relative",
+          alignItems: "center",
         }}>
-        Home
-      </Text>
-    </View>
+        <Image
+          style={{width: "100%", height: 100}}
+          // resizeMode="contain"
+          source={{
+            uri: "https://cdn.discordapp.com/attachments/1040967489288421476/1090249059513675776/pizza.jpg",
+          }}
+        />
+        <Button
+          title="ORDER"
+          buttonStyle={{
+            backgroundColor: "rgba(78, 116, 289, 1)",
+            borderRadius: 3,
+          }}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+          }}
+        />
+      </View>
+    </Card>
   );
 }
 
