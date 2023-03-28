8be1eedb10065f25aff571d01db628928a5544fb
pöggers pc <t1huhi00@students.oamk.fi>
2023-03-28 14:51:03 +0300

    fix gradle

--- frontend/src/components/axios.js
+++ frontend/src/components/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//import {API_URL} from "@env";
-const API_URL="http://192.168.1.108:3000"
+import {API_URL} from "@env";
+
 axios.defaults.baseURL = API_URL;
 
 export default axios;
