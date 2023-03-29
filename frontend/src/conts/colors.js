c4177108a08173ee2b52f1a26696674df649329f
JoniPahikainen <joni.pahikainen01@gmail.com>
2023-03-29 23:26:16 +0300

    clean color.js

--- frontend/src/conts/colors.js
+++ frontend/src/conts/colors.js
@@ -1,29 +1,3 @@
-// const lightColors = {
-//   primary: '#42C2FF',
-//   secondary: '#85F4FF',
-//   tertiary: '#B8FFF9',
-//   quaternary: '#EFFFFD',
-//   white: '#FFFFFF',
-//   black: '#000000',
-//   gray: '#BDBDBD',
-//   red: '#FF0000',
-// };
-
-// const darkColors = {
-//   primary: '#6B728E',
-//   secondary: '#50577A',
-//   tertiary: '#474E68',
-//   quaternary: '#404258',
-//   white: '#FFFFFF',
-//   black: '#000000',
-//   gray: '#BDBDBD',
-//   red: '#FF0000',
-// };
-
-// const asd = 'dark';
-// const COLORS = asd === 'light' ? lightColors : darkColors;
-// export default COLORS;
-
 export const COLORS = {
   light: {
     primary: '#42C2FF',
@@ -45,12 +19,4 @@ export const COLORS = {
     gray: '#BDBDBD',
     red: '#FF0000',
   },
-  primary: '#6B728E',
-  secondary: '#50577A',
-  tertiary: '#474E68',
-  quaternary: '#404258',
-  white: '#FFFFFF',
-  black: '#000000',
-  gray: '#BDBDBD',
-  red: '#FF0000',
 };
