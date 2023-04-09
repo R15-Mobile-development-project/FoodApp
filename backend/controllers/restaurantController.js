b2f95adf5523406655d72d7ce911844ccac9d659
JoniPahikainen <joni.pahikainen01@gmail.com>
2023-04-09 19:08:39 +0300

    add error to description length

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -6,6 +6,11 @@ const restaurantAdd = (req, res) => {
 
   if (!name || !description || !address || !image || !menus) {
     return res.status(400).json({ message: "Please fill in all fields" });
+  } else if (description.length > 255) {
+    const excessLength = description.length - 255;
+    return res.status(400).json({
+      message: `Description exceeds maximum length by ${excessLength} characters`,
+    });
   } else {
     restaurant.add(
       name,
