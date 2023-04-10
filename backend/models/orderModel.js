16f1b2aabc9398e46431fabff2ffeb85179cf01f
Keson <kesonjus@gmail.com>
2023-04-10 19:58:59 +0300

    modify db query

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const orders = {
 
     getOrders: function (id, cb) {
-        return db.query("SELECT * FROM orders WHERE user_id = ?", [id], cb)
+        return db.query("select orders.*, restaurants.name as restaurant_name from orders join restaurants on restaurants.restaurant_id = orders.restaurant_id where orders.user_id = ?;", [id], cb)
     },
 
 }
