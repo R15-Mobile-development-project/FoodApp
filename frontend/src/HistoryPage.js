b37432fb2f6c3c8729dbb011ad4175b303befc84
Keson <kesonjus@gmail.com>
2023-04-10 20:00:06 +0300

    add history incrementation and styling

--- frontend/src/HistoryPage.js
+++ frontend/src/HistoryPage.js
@@ -7,20 +7,13 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { COLORS } from './conts/colors';
 import { ThemeContext } from './components/ThemeContext';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const users = [
-  {
-    name: 'brynn',
-  },
-];
 function HistoryPage() {
   const [token, setToken] = useState(null);
-  const [price, setPrice] = useState([]);
-  const [restaurant, setRestaurant] = useState([]);
-  const [item, setItem] = useState([]);
+  const [arrayCount, setArrayCount] = useState([]);
   const navigation = useNavigation();
   const { theme, setTheme, toggleTheme } = useContext(ThemeContext);
-  var arrayCount = [];
 
   useEffect(() => {
     const FetchOrder = async () => {
@@ -37,14 +30,7 @@ function HistoryPage() {
               headers: { Authorization: `Bearer ${_token}` },
             })
             .then(response => {
-              console.log(response.data);
-              console.log(arrayCount, 'dasdasdasd');
-              arrayCount = response.data;
-              for (let i = 0; i < response.data.length; i++) {
-                setPrice([response.data[i].price]);
-                setRestaurant([response.data[i].restaurant_id]);
-                setItem([response.data[i].order_id]);
-              }
+              setArrayCount(response.data);
             })
             .catch(err => {
               console.log(err);
@@ -60,74 +46,33 @@ function HistoryPage() {
       FetchOrder();
     });
   }, [navigation]);
-  console.log(arrayCount)
 
   return (
-    <KeyboardAvoidingView
-      style={[{ flex: 1, backgroundColor: COLORS[theme].quaternary }]}>
+    <ScrollView
+      style={[{ backgroundColor: COLORS[theme].quaternary }]}>
 
       {arrayCount.map((item, index) => (
         <Card containerStyle={{
           justifyContent: 'center',
-          alignItems: 'center',
           backgroundColor: COLORS[theme].primary,
           borderRadius: 5,
           borderColor: COLORS[theme].primary
         }}>
           <Card.Title>
-            <Text style={{}}>{index}{item.restaurant}</Text>
+            <Text style={{ color: COLORS[theme].quaternary }}>{item.restaurant_name}</Text>
           </Card.Title>
-          <Card.Divider />
+          <Card.Divider style={{ borderBottomColor: COLORS[theme].quaternary, borderBottomWidth: 1 }} />
           <View style={{ flexDirection: 'row' }}>
-            <View style={{ marginLeft: 10 }}>
-              <Text>
-                {item.item}------------------
-              </Text>
+            <View>
+              <Text style={{ color: COLORS[theme].quaternary, textAlign: 'left' }}>{item.order_id}</Text>
             </View>
-            <View style={{ marginRight: 10 }}>
-              <Text>
-                {item.price}€
-              </Text>
+            <View style={{ flex: 1 }}>
+              <Text style={{ color: COLORS[theme].quaternary, textAlign: 'right' }}>{item.price}€</Text>
             </View>
           </View>
         </Card>
       ))}
-
-      {/* style={[{flex: 1, backgroundColor: COLORS[theme].quaternary}]}>
-      <Card
-        containerStyle={{
-          justifyContent: 'center',
-          backgroundColor: COLORS[theme].primary,
-          borderRadius: 5,
-          borderColor: COLORS[theme].primary,
-        }}>
-        <Card.Title>
-          <Text style={{color: COLORS[theme].quaternary}}>{restaurant}</Text>
-        </Card.Title>
-        <Card.Divider />
-        <View style={{flexDirection: 'row'}}>
-          <View style={{flex: 1}}>
-            <Text style={{color: COLORS[theme].quaternary}} textAlign="left">
-              {item}
-            </Text>
-          </View>
-          <View>
-            <Text style={{color: COLORS[theme].quaternary}} textAlign="right">
-              {price}€
-            </Text>
-          </View>
-        </View>
-      </Card>
-      <Card style={{}}>
-        <Card.Title>{restaurant}</Card.Title>
-        <Card.Divider />
-        <View>
-          <Text>
-            {item}--------------{price}€
-          </Text>
-        </View>
-      </Card> */}
-    </KeyboardAvoidingView>
+    </ScrollView>
   );
 
 }
