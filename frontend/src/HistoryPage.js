b7700d292bae8b8144d6dcee274a73bed3e7d1cf
Tommukka <laine.tomi01@gmail.com>
2023-04-13 15:09:21 +0300

    Add restaurant cards to homepage

--- frontend/src/HistoryPage.js
+++ frontend/src/HistoryPage.js
@@ -1,33 +1,33 @@
-import { View, Text } from 'react-native';
-import styles from './conts/Styles';
-import { Card } from '@rneui/themed';
-import axios from './components/axios';
-import EncryptedStorage from 'react-native-encrypted-storage';
-import React, { useState, useEffect, useContext } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { COLORS } from './conts/colors';
-import { ThemeContext } from './components/ThemeContext';
-import { ScrollView } from 'react-native-gesture-handler';
+import {View, Text} from "react-native";
+import styles from "./conts/Styles";
+import {Card} from "@rneui/themed";
+import axios from "./components/axios";
+import EncryptedStorage from "react-native-encrypted-storage";
+import React, {useState, useEffect, useContext} from "react";
+import {useNavigation} from "@react-navigation/native";
+import {COLORS} from "./conts/colors";
+import {ThemeContext} from "./components/ThemeContext";
+import {ScrollView} from "react-native-gesture-handler";
 
 function HistoryPage() {
   const [token, setToken] = useState(null);
   const [arrayCount, setArrayCount] = useState([]);
   const navigation = useNavigation();
-  const { theme, setTheme, toggleTheme } = useContext(ThemeContext);
+  const {theme, setTheme, toggleTheme} = useContext(ThemeContext);
 
   useEffect(() => {
     const FetchOrder = async () => {
       try {
-        const _token = await EncryptedStorage.getItem('token');
+        const _token = await EncryptedStorage.getItem("token");
 
         if (_token !== undefined) {
           setToken(_token);
 
-          const headers = { headers: { Authorization: `Bearer ${_token}` } };
+          const headers = {headers: {Authorization: `Bearer ${_token}`}};
 
           axios
-            .get('/order', {
-              headers: { Authorization: `Bearer ${_token}` },
+            .get("/order", {
+              headers: {Authorization: `Bearer ${_token}`},
             })
             .then(response => {
               setArrayCount(response.data);
@@ -36,45 +36,56 @@ function HistoryPage() {
               console.log(err);
             });
         } else {
-          console.log('No token found');
+          console.log("No token found");
         }
       } catch (error) {
         console.log(error);
       }
     };
-    navigation.addListener('focus', () => {
+    navigation.addListener("focus", () => {
       FetchOrder();
     });
   }, [navigation]);
 
   return (
-    <ScrollView
-      style={[{ backgroundColor: COLORS[theme].quaternary }]}>
-
+    <ScrollView style={[{backgroundColor: COLORS[theme].quaternary}]}>
       {arrayCount.map((item, index) => (
-        <Card containerStyle={{
-          justifyContent: 'center',
-          backgroundColor: COLORS[theme].primary,
-          borderRadius: 5,
-          borderColor: COLORS[theme].primary
-        }}>
+        <Card
+          containerStyle={{
+            justifyContent: "center",
+            backgroundColor: COLORS[theme].primary,
+            borderRadius: 5,
+            borderColor: COLORS[theme].primary,
+          }}>
           <Card.Title>
-            <Text style={{ color: COLORS[theme].quaternary }}>{item.restaurant_name}</Text>
+            <Text style={{color: COLORS[theme].quaternary}}>
+              {item.restaurant_name}
+            </Text>
           </Card.Title>
-          <Card.Divider style={{ borderBottomColor: COLORS[theme].quaternary, borderBottomWidth: 1 }} />
-          <View style={{ flexDirection: 'row' }}>
+          <Card.Divider
+            style={{
+              borderBottomColor: COLORS[theme].quaternary,
+              borderBottomWidth: 1,
+            }}
+          />
+          <View style={{flexDirection: "row"}}>
             <View>
-              <Text style={{ color: COLORS[theme].quaternary, textAlign: 'left' }}>{item.order_id}</Text>
+              <Text
+                style={{color: COLORS[theme].quaternary, textAlign: "left"}}>
+                {item.order_id}
+              </Text>
             </View>
-            <View style={{ flex: 1 }}>
-              <Text style={{ color: COLORS[theme].quaternary, textAlign: 'right' }}>{item.price}€</Text>
+            <View style={{flex: 1}}>
+              <Text
+                style={{color: COLORS[theme].quaternary, textAlign: "right"}}>
+                {item.price}€
+              </Text>
             </View>
           </View>
         </Card>
       ))}
     </ScrollView>
   );
-
 }
 
 export default HistoryPage;
--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,41 +1,98 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
 import {View, Text, Image} from "react-native";
-import COLORS from "./conts/colors";
+import {COLORS} from "./conts/colors";
 import styles from "./conts/Styles";
 import {Card, ListItem, Button, Icon} from "@rneui/themed";
+import axios from "./components/axios";
+import EncryptedStorage from "react-native-encrypted-storage";
+import {ThemeContext} from "./components/ThemeContext";
+import {useNavigation} from "@react-navigation/native";
+import {ScrollView} from "react-native-gesture-handler";
 
 function HomePage() {
-  //const {theme} = useContext(ThemeContext);
+  const navigation = useNavigation();
+  const {theme, setTheme, toggleTheme} = useContext(ThemeContext);
+  const [token, setToken] = useState(null);
+  const [arrayCount, setArrayCount] = useState([]);
+
+  useEffect(() => {
+    const FetchRestaurant = async () => {
+      try {
+        const _token = await EncryptedStorage.getItem("token");
+
+        if (_token !== undefined) {
+          setToken(_token);
+
+          const headers = {headers: {Authorization: `Bearer ${_token}`}};
+
+          axios
+            .get("/restaurant", {
+              headers,
+            })
+            .then(response => {
+              setArrayCount(response.data);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    navigation.addListener("focus", () => {
+      FetchRestaurant();
+    });
+  }, [navigation]);
+
   return (
-    <Card containerStyle={{}} wrapperStyle={{}}>
-      <Card.Title>RESTAURANT NAME</Card.Title>
-      <Card.Divider />
-      <View
-        style={{
-          position: "relative",
-          alignItems: "center",
-        }}>
-        <Image
-          style={{width: "100%", height: 100}}
-          // resizeMode="contain"
-          source={{
-            uri: "https://cdn.discordapp.com/attachments/1040967489288421476/1090249059513675776/pizza.jpg",
-          }}
-        />
-        <Button
-          title="ORDER"
-          buttonStyle={{
-            backgroundColor: "rgba(78, 116, 289, 1)",
-            borderRadius: 3,
-          }}
-          containerStyle={{
-            width: 200,
-            marginHorizontal: 50,
-            marginVertical: 10,
-          }}
-        />
-      </View>
-    </Card>
+    <>
+      <ScrollView style={[{backgroundColor: COLORS[theme].quaternary}]}>
+        {arrayCount.map((item, index) => (
+          <Card
+            containerStyle={{
+              justifyContent: "center",
+              backgroundColor: COLORS[theme].primary,
+              borderRadius: 5,
+              borderColor: COLORS[theme].primary,
+            }}>
+            <Card.Title>
+              <Text style={{color: COLORS[theme].quaternary, fontSize: 20}}>
+                {item.name}
+              </Text>
+            </Card.Title>
+            <Card.Divider
+              style={{
+                borderBottomColor: COLORS[theme].quaternary,
+                borderBottomWidth: 1,
+              }}
+            />
+            <View style={{alignItems: "center", marginBottom: -10}}>
+              <Image
+                style={{width: "100%", height: 100, borderRadius: 5}}
+                // resizeMode="contain"
+                source={{
+                  uri: item.image,
+                }}
+              />
+              <Button
+                title="ORDER"
+                titleStyle={{color: COLORS[theme].primary}}
+                buttonStyle={{
+                  backgroundColor: COLORS[theme].quaternary,
+                  borderRadius: 3,
+                }}
+                containerStyle={{
+                  width: "100%",
+                  // marginHorizontal: 50,
+                  marginVertical: 10,
+                }}
+              />
+            </View>
+          </Card>
+        ))}
+      </ScrollView>
+    </>
   );
 }
 
