cf566a67cd5ed5ec45908f9f4d9da3f476530533
Tommukka <laine.tomi01@gmail.com>
2023-04-14 00:55:08 +0300

    Add restaurant route for all restaurants and rename the route for current user's own restaurant

--- backend/routes/restaurantRoutes.js
+++ backend/routes/restaurantRoutes.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const restaurantController = require("../controllers/restaurantController");
 
 router.post("/add", jwt.verifyToken, restaurantController.restaurantAdd);
-router.get("/", jwt.verifyToken, restaurantController.restaurantGetByUserId);
+router.get("/", restaurantController.restaurantList);
+router.get("/me", jwt.verifyToken, restaurantController.restaurantGetByUserId);
 router.put("/update", jwt.verifyToken, restaurantController.restaurantUpdate);
 
 module.exports = router;
