d6ebc76dede32ad6be73cb6e19cbc119cf72d754
Keson <kesonjus@gmail.com>
2023-04-16 22:09:14 +0300

    clean unused imports

--- frontend/src/HistoryPage.js
+++ frontend/src/HistoryPage.js
@@ -1,19 +1,18 @@
-import {View, Text} from "react-native";
-import styles from "./conts/Styles";
-import {Card} from "@rneui/themed";
-import axios from "./components/axios";
-import EncryptedStorage from "react-native-encrypted-storage";
-import React, {useState, useEffect, useContext} from "react";
-import {useNavigation} from "@react-navigation/native";
-import {COLORS} from "./conts/colors";
-import {ThemeContext} from "./components/ThemeContext";
-import {ScrollView} from "react-native-gesture-handler";
+import { View, Text } from 'react-native';
+import { Card } from '@rneui/themed';
+import axios from './components/axios';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { COLORS } from './conts/colors';
+import { ThemeContext } from './components/ThemeContext';
+import { ScrollView } from 'react-native-gesture-handler';
 
 function HistoryPage() {
   const [token, setToken] = useState(null);
   const [arrayCount, setArrayCount] = useState([]);
   const navigation = useNavigation();
-  const {theme, setTheme, toggleTheme} = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
 
   useEffect(() => {
     const FetchOrder = async () => {
@@ -23,11 +22,11 @@ function HistoryPage() {
         if (_token !== undefined) {
           setToken(_token);
 
-          const headers = {headers: {Authorization: `Bearer ${_token}`}};
+          const headers = { headers: { Authorization: `Bearer ${_token}` } };
 
           axios
             .get("/order", {
-              headers: {Authorization: `Bearer ${_token}`},
+              headers: { Authorization: `Bearer ${_token}` },
             })
             .then(response => {
               setArrayCount(response.data);
@@ -49,7 +48,7 @@ function HistoryPage() {
 
   return (
     <>
-      <ScrollView style={[{backgroundColor: COLORS[theme].quaternary}]}>
+      <ScrollView style={[{ backgroundColor: COLORS[theme].quaternary }]}>
         {arrayCount.map((item, index) => (
           <Card
             key={index}
@@ -60,7 +59,7 @@ function HistoryPage() {
               borderColor: COLORS[theme].primary,
             }}>
             <Card.Title>
-              <Text style={{color: COLORS[theme].quaternary}}>
+              <Text style={{ color: COLORS[theme].quaternary }}>
                 {item.restaurant_name}
               </Text>
             </Card.Title>
@@ -70,16 +69,16 @@ function HistoryPage() {
                 borderBottomWidth: 1,
               }}
             />
-            <View style={{flexDirection: "row"}}>
+            <View style={{ flexDirection: "row" }}>
               <View>
                 <Text
-                  style={{color: COLORS[theme].quaternary, textAlign: "left"}}>
+                  style={{ color: COLORS[theme].quaternary, textAlign: "left" }}>
                   {item.order_id}
                 </Text>
               </View>
-              <View style={{flex: 1}}>
+              <View style={{ flex: 1 }}>
                 <Text
-                  style={{color: COLORS[theme].quaternary, textAlign: "right"}}>
+                  style={{ color: COLORS[theme].quaternary, textAlign: "right" }}>
                   {item.price}€
                 </Text>
               </View>
