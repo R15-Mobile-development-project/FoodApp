f93ad187c178b4aaf929552ad954b74e9530ed32
JoniPahikainen <joni.pahikainen01@gmail.com>
2023-04-18 14:27:11 +0300

    add comments conrollers

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,25 +1,35 @@
+// Require the 'orderModel' module, which exports functions for interacting with order data.
 const order = require("../models/orderModel");
+// Require the 'bcrypt' package, which is used for hashing passwords.
 const bcrypt = require("bcrypt");
+// Require the 'jwt' module, which exports functions for handling JSON Web Tokens.
 const jwt = require("../config/jwt");
+// Require the 'dotenv' package to access environment variables.
 require("dotenv").config();
 
+// Handler function for retrieving a user's orders.
 const getOrders = (req, res) => {
-    order.getOrders(req.userId, (err, results) => {
-        if (err) {
-            return res.status(500).json({
-                message: "Error occured",
-            });
-        }
-        if (results.length === 0) {
-            return res.status(400).json({
-                message: "No order data was found for that user",
-            });
-        }
-        console.log(results);
-        res.json(results);
-    });
+  // Call the 'getOrders' function from the 'orderModel' module, passing in the user ID stored in the request object.
+  order.getOrders(req.userId, (err, results) => {
+    if (err) {
+      // If an error occurred, return a 500 Internal Server Error status with an error message.
+      return res.status(500).json({
+        message: "Error occurred",
+      });
+    }
+    if (results.length === 0) {
+      // If no orders were found for the user, return a 400 Bad Request status with a message.
+      return res.status(400).json({
+        message: "No order data was found for that user",
+      });
+    }
+    // If orders were found, log them to the console for debugging purposes and send them as a JSON response.
+    console.log(results);
+    res.json(results);
+  });
 };
 
+// Export the 'getOrders' function for use in other modules.
 module.exports = {
-    getOrders,
+  getOrders,
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,29 +4,37 @@ const emailvalidator = require("email-validator");
 const jwt = require("../config/jwt");
 require("dotenv").config();
 
+// Function for handling user login
 const userLogin = (req, res) => {
+  // Extract email and password from request body
   const { email, password } = req.body;
 
+  // Validate email and password
   if (!emailvalidator.validate(email) || !password) {
     return res.status(401).json({ message: "Invalid credentials" });
   }
 
+  // Get user data by email
   user.getByEmail(email, (err, results) => {
+    // Handle errors during query
     if (err) {
       return res.status(500).json({
         message: "Error occured",
       });
     }
+    // If user found, check the password
     if (results.length > 0) {
       bcrypt.compare(
         password,
         results[0].password.toString(),
         (err, result) => {
+          // Handle errors during password comparison
           if (err) {
             return res.status(500).json({
               message: "Error occured",
             });
           }
+          // If password matches, generate token and send it in response
           if (result) {
             const token = jwt.generateToken(
               results[0].user_id,
@@ -51,9 +59,12 @@ const userLogin = (req, res) => {
   });
 };
 
+// Function for handling user registration
 const userRegister = (req, res) => {
+  // Extract user details from request body
   const { fname, lname, email, password, user_type } = req.body;
 
+  // Validate user input
   if (!fname || !lname || !email || !password) {
     return res.status(400).json({ message: "Please fill in all fields" });
   } else if (!emailvalidator.validate(email)) {
@@ -62,19 +73,24 @@ const userRegister = (req, res) => {
     return res.status(400).json({ message: "Password too short" });
   }
 
+  // Check if user_type is valid
   if (user_type) {
     if (user_type !== 0 && user_type !== 1) {
       return res.status(400).json({ message: "Invalid user type" });
     }
   }
 
+  // Hash the password
   bcrypt.hash(password, 10, function (err, hash) {
+    // Handle errors during hashing
     if (err) {
       return res.status(500).json({
         message: "Error occured",
       });
     }
+    // Add user to the database
     user.add(fname, lname, email, hash, user_type, (err, results) => {
+      // Handle errors during user addition
       if (err) {
         if (err.errno === 1062) {
           return res.status(400).json({
@@ -86,6 +102,7 @@ const userRegister = (req, res) => {
           });
         }
       }
+      // Send success message on successful registration
       return res.status(200).json({
         message: "Registration successful",
       });
@@ -93,29 +110,38 @@ const userRegister = (req, res) => {
   });
 };
 
+// Function for getting user profile data
 const userProfile = (req, res) => {
+  // Get user data by user id
   user.getById(req.userId, (err, results) => {
+    // Handle errors during query
     if (err) {
       return res.status(500).json({
         message: "Error occured",
       });
     }
 
+    // If no user data found, return an error
     if (results.length === 0) {
       return res.status(400).json({
         message: "No user data was found for that user id",
       });
     }
 
+    // Remove sensitive data before sending the response
     delete results[0].user_id;
     delete results[0].password;
 
+    // Send user profile data in the response
     res.json(results[0]);
   });
 };
 
+// Function for deleting user profile
 const deleteProfile = (req, res) => {
+  // Delete user data by user id
   user.deleteById(req.userId, (err, results) => {
+    // Handle errors during deletion
     if (err) {
       console.log(err);
       return res.status(500).json({
@@ -123,9 +149,11 @@ const deleteProfile = (req, res) => {
       });
     }
 
+    // If user deleted successfully, send a success message
     if (results.affectedRows === 1) {
       return res.json({ message: "User deleted" });
     } else {
+      // If unable to delete user, send an error message
       res.status(400).json({
         message: "Unable to delete user",
       });
@@ -133,17 +161,20 @@ const deleteProfile = (req, res) => {
   });
 };
 
+// Function for updating user profile
 const updateUserProfile = async (req, res) => {
+  // Extract user details from request body
   const { fname, lname, email, password } = req.body;
 
+  // Validate email and password
   if (email && !emailvalidator.validate(email)) {
     return res.status(400).json({ message: "Invalid email" });
   } else if (password && password.length < 1) {
     return res.status(400).json({ message: "Password too short" });
   }
 
+  // If a new password is provided, hash it
   let hashedPassword;
-
   if (password) {
     try {
       hashedPassword = await bcrypt.hash(password, 10);
@@ -152,6 +183,7 @@ const updateUserProfile = async (req, res) => {
     }
   }
 
+  // Prepare data for updating user profile
   const data = {
     email: email,
     fname: fname,
@@ -160,7 +192,9 @@ const updateUserProfile = async (req, res) => {
     userId: req.userId,
   };
 
+  // Update user data by user id
   user.updateUserById(data, (err, results) => {
+    // Handle errors during update
     if (err) {
       if (err.errno === 1062) {
         return res.status(500).json({
@@ -172,6 +206,7 @@ const updateUserProfile = async (req, res) => {
       });
     }
 
+    // Send appropriate response based on the update result
     if (results.changedRows === 1) {
       return res.json({ message: "Profile updated" });
     } else if (results.changedRows === 0 && results.affectedRows === 1) {
@@ -183,18 +218,26 @@ const updateUserProfile = async (req, res) => {
     }
   });
 };
+
 const updateBalance = (req, res) => {
+  // Extract balance value from the request body
   const { balance } = req.body;
 
+  // Check if the balance value is a number
   if (typeof balance != "number") {
+    // If not, respond with a 400 status code and error message
     return res.status(400).json({ message: "Not a number" });
   }
 
+  // Prepare the data object to be passed to the user model
   const data = {
     balance: balance,
     userId: req.userId,
   };
+
+  // Call the updateBalanceById function in the user model
   user.updateBalanceById(data, (err, results) => {
+    // If there's an error, log it and respond with a 500 status code and error message
     if (err) {
       console.log(err);
       return res.status(500).json({
@@ -202,11 +245,16 @@ const updateBalance = (req, res) => {
       });
     }
 
+    // If one row has been changed, the balance was successfully updated
     if (results.changedRows === 1) {
       return res.json({ message: "Balance updated" });
-    } else if (results.changedRows === 0 && results.affectedRows === 1) {
+    }
+    // If no rows were changed but one row was affected, the balance was already up-to-date
+    else if (results.changedRows === 0 && results.affectedRows === 1) {
       return res.json({ message: "Everything up to date" });
-    } else {
+    }
+    // In any other case, respond with a 400 status code and an error message
+    else {
       res.status(400).json({
         message: "Unable to add balance",
       });
