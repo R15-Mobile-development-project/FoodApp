a13a37b3dcc8aa2e2bc1d7838ede5d4ac0d4d140
Lasse Suomela <suomelasse@gmail.com>
2023-04-19 19:33:59 +0300

    Added total price to checkout page

--- frontend/src/CheckoutPage.js
+++ frontend/src/CheckoutPage.js
@@ -1,13 +1,13 @@
 import {View, Text} from "react-native";
 import {Card, Button} from "@rneui/themed";
 import axios from "./components/axios";
-import EncryptedStorage from "react-native-encrypted-storage";
 import React, {useState, useEffect, useContext} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {COLORS} from "./conts/colors";
 import {ThemeContext} from "./components/ThemeContext";
 import {ScrollView} from "react-native-gesture-handler";
 import {LogBox} from "react-native";
+import {GetToken} from "./components/Token";
 
 LogBox.ignoreLogs([
   "Non-serializable values were found in the navigation state",
@@ -17,33 +17,37 @@ function CheckoutPage({route}) {
   const navigation = useNavigation();
 
   const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
   const {theme} = useContext(ThemeContext);
   const {restaurant_id, cartItems} = route.params;
 
   useEffect(() => {
     setCart(cartItems);
+
+    let price = 0.0;
+    Array.from(cartItems).forEach(item => {
+      price += item.price;
+    });
+
+    setTotal(price);
   }, []);
 
-  /* const CreateOrder = async () => {
-    try {
-      axios
-        .post(
-          `/restaurant/${restaurant_id}/order`,
-          {},
-          {
-            headers: {Authorization: `Bearer ${token}`},
-          },
-        )
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  }; */
+  const CreateOrder = async () => {
+    const token = await GetToken();
+    const headers = {headers: {Authorization: `Bearer ${token}`}};
+
+    axios
+      .post(`/restaurant/${restaurant_id}/order`, headers)
+      .then(response => {
+        // on success navigate somewhere and show status msg
+        console.log(response.data);
+      })
+      .catch(err => {
+        // on error show error msg
+        console.log(err);
+      });
+  };
 
   return (
     <>
@@ -75,6 +79,19 @@ function CheckoutPage({route}) {
         ))}
       </ScrollView>
 
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: COLORS[theme].quaternary,
+          paddingBottom: 10,
+          paddingTop: 10,
+        }}>
+        <Text style={{color: COLORS[theme].primary, fontSize: 30}}>
+          Total: {total.toFixed(2)} €
+        </Text>
+      </View>
+
       <Button
         title="PAY"
         buttonStyle={{
@@ -89,12 +106,9 @@ function CheckoutPage({route}) {
           fontSize: 20,
           fontWeight: "bold",
         }}
-        onPress={() =>
-          navigation.navigate("Checkout", {
-            restaurant_id: restaurant_id,
-            cartItems: cart,
-          })
-        }
+        onPress={() => {
+          CreateOrder();
+        }}
       />
     </>
   );
