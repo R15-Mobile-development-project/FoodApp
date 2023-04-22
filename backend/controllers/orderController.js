235e52bca07c6ead23b3586d789ecbc7e56d378a
Tommukka <laine.tomi01@gmail.com>
2023-04-22 18:40:17 +0300

    Return all items in an order as a single array

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,35 +1,57 @@
-// Require the 'orderModel' module, which exports functions for interacting with order data.
 const order = require("../models/orderModel");
-// Require the 'bcrypt' package, which is used for hashing passwords.
-const bcrypt = require("bcrypt");
-// Require the 'jwt' module, which exports functions for handling JSON Web Tokens.
-const jwt = require("../config/jwt");
-// Require the 'dotenv' package to access environment variables.
 require("dotenv").config();
 
-// Handler function for retrieving a user's orders.
 const getOrders = (req, res) => {
-  // Call the 'getOrders' function from the 'orderModel' module, passing in the user ID stored in the request object.
+  const orderMap = new Map();
   order.getOrders(req.userId, (err, results) => {
     if (err) {
-      // If an error occurred, return a 500 Internal Server Error status with an error message.
       return res.status(500).json({
         message: "Error occurred",
       });
     }
     if (results.length === 0) {
-      // If no orders were found for the user, return a 400 Bad Request status with a message.
       return res.status(400).json({
         message: "No order data was found for that user",
       });
     }
-    // If orders were found, log them to the console for debugging purposes and send them as a JSON response.
-    console.log(results);
-    res.json(results);
+
+    results.forEach((entry) => {
+      const {
+        order_id,
+        date,
+        price,
+        status,
+        restaurant_id,
+        user_id,
+        name,
+        item_price,
+        restaurant_name,
+      } = entry;
+
+      if (!orderMap.has(order_id)) {
+        orderMap.set(order_id, {
+          order_id,
+          date,
+          price,
+          status,
+          restaurant_id,
+          user_id,
+          restaurant_name,
+          items: [],
+        });
+      }
+
+      const order = orderMap.get(order_id);
+      order.items.push({
+        name,
+        item_price,
+      });
+    });
+    console.log(Array.from(orderMap.values()));
+    return res.status(200).json(Array.from(orderMap.values()));
   });
 };
 
-// Export the 'getOrders' function for use in other modules.
 module.exports = {
   getOrders,
 };
